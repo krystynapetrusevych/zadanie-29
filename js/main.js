7bfbd29ce951b171dc36705e1ba87dfdddee456e
krystynapetrusevych <petrusevych.k@icloud.com>
2023-05-30 17:33:44 +0200

    js suma zmiennych

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let firstNumber = 5;
+let secondNumber = 6;
+function wynik(firstNumber,secondNumber) {
+    let result = firstNumber + secondNumber
+    return result;
+}
+console.log(wynik (firstNumber,secondNumber));
